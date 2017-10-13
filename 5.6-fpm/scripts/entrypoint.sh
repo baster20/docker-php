5abf574605023792a01e7dcb5a727f8ec96a6168
Roman A Moiseev <roman.a.moiseev@gmail.com>
2017-10-13 15:51:47 +0300

    7.2

--- 5.6-fpm/scripts/entrypoint.sh
+++ 5.6-fpm/scripts/entrypoint.sh
@@ -16,35 +16,46 @@ fi
 
 # Setup git variables
 if [ ! -z "$GIT_EMAIL" ]; then
+  echo "config email"
   git config --global user.email "$GIT_EMAIL"
 fi
 if [ ! -z "$GIT_NAME" ]; then
-  git config --global user.name "$GIT_NAME"
-  git config --global push.default simple
+  echo "config name"
+   git config --global user.name "$GIT_NAME"
+   git config --global push.default simple
 fi
 
 # Dont pull code down if the .git folder exists
-if [ -d "/var/www/html/.git" ]; then
+if [ ! -d "/var/www/html/.git" ]; then
   # Clone code from git for our site!
-  if [ ! -z "$GIT_REPO" ]; then
+  if [ ! -z "$GIT_SOURCE" ]; then
+    echo "git source variable found"
     rm -Rf /var/www/html/*
     if [ ! -z "$GIT_BRANCH" ]; then
-      git clone -b $GIT_BRANCH $GIT_REPO /var/www/html
+      echo "cloning branch"
+      git clone -b $GIT_BRANCH $GIT_SOURCE /var/www/html
     else
-      git clone $GIT_REPO /var/www/html
+      echo "cloning without branch"
+      git clone $GIT_SOURCE /var/www/html
     fi
   fi
 else
   # Pull down code from git for our site!
-  git pull
+  echo "pulling"
+  cd /var/www/html && git pull
 fi
+
+chown -R www-data:www-data /var/www
 
 # Install composer dependenncies
 if [ -f "/var/www/html/composer.json" ]; then
+   echo "found composer json"
   if [ -d "/var/www/html/vendor" ]; then
-    composer update
+    echo "found vendor dir, doing update"
+    sudo -u www-data composer update
   else
-    composer install
+    echo "doing composer install"
+    sudo -u www-data composer install
   fi 
 fi
 
